a28a67799a4bd60358fa83c3130d0e3dae8c70a6
katie <katieanriley@gmail.com>
2025-07-25 13:01:27 -0700

    add tapestry-react, basic style

--- app/javascript/components/hello_controller.jsx
+++ app/javascript/components/hello_controller.jsx
@@ -1,5 +1,28 @@
 import React from "react"
+import {
+  Card,
+  Field,
+  Heading,
+  List,
+  StackView,
+} from '@planningcenter/tapestry-react'
+import _ from 'lodash'
 
-export default function HelloComponent({ name }) {
-  return <h1>Hello, {name || "World"}!</h1>
+export default function HelloComponent({  whiskies, locations  }) {
+  return (
+    <StackView padding={2}>
+      <StackView>
+        <Heading>Hello, lets take a look at all of your whiskies you have tried:</Heading>
+        <Card
+          as={List}
+          marginTop={1}
+          onItemRemoveRequest={(index) => alert('Delete item at index ' + index)}
+        >
+          {whiskies.map((whisky) => (
+            <List.Item>{whisky.name}, you tasting notes were: {whisky.tasting_notes}</List.Item>
+          ))}
+        </Card>
+      </StackView>
+    </StackView>
+  )
 }
--- app/javascript/entrypoints/application.jsx
+++ app/javascript/entrypoints/application.jsx
@@ -1,15 +1,44 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
 import HelloComponent from "../components/hello_controller"
+import { ThemeProvider } from "@planningcenter/tapestry-react" 
 
 console.log("Vite ⚡️ Rails")
 
 function mountReactRoot() {
-  const reactRoot = document.getElementById("react-root")
-  if (!reactRoot) return
+  const whiskyNode = document.getElementById("whisky");
+  const locationNode = document.getElementById("location");
+  const appRoot = document.getElementById("app"); // <- define this
 
-  const props = JSON.parse(reactRoot.dataset.reactProps || "{}")
-  createRoot(reactRoot).render(<HelloComponent {...props} />)
+  const whiskies = whiskyNode ? JSON.parse(whiskyNode.dataset.reactProps) : [];
+  const locations = locationNode ? JSON.parse(locationNode.dataset.reactProps) : [];
+
+  const props = {
+    whiskies: whiskies.whiskies || [],
+    locations: locations.locations || [],
+  }
+
+  const theme = {
+    boxSizes: {
+      small: { paddingHorizontal: 1, paddingVertical: 0.75 },
+      medium: { paddingHorizontal: 1.5, paddingVertical: 1 },
+    },
+    breakpoints: {
+      phone: 480,
+      tablet: 720,
+      desktop: 960,
+    },
+    colors: {
+      primary: 'hotpink',
+      secondary: 'lime',
+    },
+  }
+
+  createRoot(appRoot).render(
+    <ThemeProvider theme={theme}>
+      <HelloComponent {...props} />
+    </ThemeProvider>
+  )
 }
 
 mountReactRoot()
